a3d5ac2a2c824ac6d09aba94af5eaf10113e0457
Martijn de Vos <mdmartijndevos@gmail.com>
2012-11-07 16:18:04 +0100

    added more unit tests

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by M.A. de Vos on 07-11-12.
+//  Copyright (c) 2012 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
