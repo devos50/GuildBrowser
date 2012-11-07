58a8b798c5d81ff23e16fa94629d6fe4a10f46df
Martijn de Vos <mdmartijndevos@gmail.com>
2012-11-07 15:50:19 +0100

    added unit tests

---
+++ WowUtilsTests.h
@@ -0,0 +1,14 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by M.A. de Vos on 07-11-12.
+//  Copyright (c) 2012 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import "WoWUtils.h"
+
+@interface WowUtilsTests : SenTestCase
+
+@end
